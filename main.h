a5fd94ef19c1fa83841262b9645132982535d61e
Beaumois <louis.beaumois@etudiant.univrennes1.fr>
2024-07-22 14:51:07 +0200

    completing main.h with some prototypes and a structure printer

--- main.h
+++ main.h
@@ -1,4 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+
+int _putchar(char c);
+
+int _strlen(const char *str);
+
+int _printf(const char *format, ...);
+
+void _printChar(va_list arg);
+
+void _printString(va_list arg);
+
+void _printPercent();
+
+typedef struct printer
+{
+	char format;
+	void (*func)(va_list arg);
+} print_t;
 
 #endif
