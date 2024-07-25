2b6ada3b3ef768f8a3f7c8e7966d4e0f4f2d2bdf
SDINAHET <stephane.dinahet@gmail.com>
2024-07-25 12:53:06 +0000

    add description int _printUnsignedInt(va_list args) and fix bug betty

--- 9_printUnsignedInt.c
+++ 9_printUnsignedInt.c
@@ -2,34 +2,45 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * _printUnsignedInt - Prints an unsigned integer
+ * @args: A va_list containing the unsigned integer to print
+ *
+ * Return: The number of characters printed
+ */
 int _printUnsignedInt(va_list args)
 {
-    unsigned int num = va_arg(args, unsigned int);
-    char buffer[20];  /*is big for content max unsigned int*/
-    int i = 0, j, len;
-    char temp;
+	unsigned int num = va_arg(args, unsigned int);
+	char buffer[20];  /*is big for content max unsigned int*/
+	int i = 0, j, len;
+	char temp;
 
-    /*cast entier to string*/
-    if (num == 0) {
-        buffer[i++] = '0';
-    } else {
-        while (num != 0) {
-            buffer[i++] = (num % 10) + '0';
-            num /= 10;
-        }
-    }
-    buffer[i] = '\0';
+	/*cast entier to string*/
+	if (num == 0)
+	{
+		buffer[i++] = '0';
+	}
+	else
+	{
+		while (num != 0)
+		{
+			buffer[i++] = (num % 10) + '0';
+			num /= 10;
+		}
+	}
+	buffer[i] = '\0';
 
-    /*reverse string*/
-    len = i;
-    for (j = 0; j < len / 2; j++) {
-        temp = buffer[j];
-        buffer[j] = buffer[len - j - 1];
-        buffer[len - j - 1] = temp;
-    }
+	/*reverse string*/
+	len = i;
+	for (j = 0; j < len / 2; j++)
+	{
+		temp = buffer[j];
+		buffer[j] = buffer[len - j - 1];
+		buffer[len - j - 1] = temp;
+	}
 
-    /*print string*/
-    write(1, buffer, len);
+	/*print string*/
+	write(1, buffer, len);
 
-    return len;  /*return count of string*/
+	return (len);  /*return count of string*/
 }
