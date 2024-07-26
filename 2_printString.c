541b35a9b71badbdd0162fadf8a3af9ee370b19d
SDINAHET <stephane.dinahet@gmail.com>
2024-07-26 20:56:58 +0000

    fix free malloc for 2_printString.c

--- 2_printString.c
+++ 2_printString.c
@@ -1,25 +1,39 @@
-#include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
 
 /**
- * _printString - Prints a string.
- * @args: List of arguments containing the string to print.
+ * _printString - Prints a string to stdout.
+ * @args: The list of arguments.
  *
  * Return: The number of characters printed.
  */
 int _printString(va_list args)
 {
 	char *str = va_arg(args, char *);
+	char *ptr;
+	char *str_copy;
 	int count = 0;
 
 	if (str == NULL)
 		str = "(null)";
 
-	while (*str)
+	str_copy = (char *)malloc(strlen(str) + 1);
+	if (str_copy == NULL)
+		return (0);
+
+	strcpy(str_copy, str);
+	ptr = str_copy;
+
+	while (*ptr)
 	{
-		count += _putchar(*str);
-		str++;
+		count += _putchar(*ptr);
+		ptr++;
 	}
+
+	free(str_copy);
 
 	return (count);
 }
