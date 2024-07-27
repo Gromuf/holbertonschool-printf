660aab0988543a94d397631cbda5e16bacc6aa8a
SDINAHET <stephane.dinahet@gmail.com>
2024-07-27 17:02:18 +0200

    'ajout fichier 27/07/2024+ save file spe et _printf for manual revu'

---
+++ save_file_27_07_2024_16_00/_strcopy.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strcopy - Copies a string to a newly allocated memory.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the newly allocated string, or NULL on failure.
+ */
+char *_strcopy(const char *src)
+{
+    char *dest;
+    size_t len;
+
+    if (src == NULL)
+        return NULL;
+
+    len = strlen(src) + 1; /* +1 for the null terminator */
+    dest = malloc(len);
+    if (dest == NULL)
+        return NULL;
+
+    memcpy(dest, src, len);
+    return dest;
+}
